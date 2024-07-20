e64dfea29cb500ac7aebda900a46920595cdf1c4
rasubramanian <v-rasubramanian@expediagroup.com>
2024-07-20 20:19:43 +0530

    component folder structure

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import LineChart from "../../components/dashboard/line-chart";
 
 const Dashboard = () => {
   return (
     <div>
       <h1>Dashboard</h1>
+      <LineChart />
     </div>
   );
 };
