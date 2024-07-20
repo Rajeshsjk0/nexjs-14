50b2f6040e0a27995b5f783d2aea6e282f8e717a
rasubramanian <v-rasubramanian@expediagroup.com>
2024-07-20 22:27:36 +0530

    dynamic metaData of routes

--- src/app/(routes)/products/[productId]/page.tsx
+++ src/app/(routes)/products/[productId]/page.tsx
@@ -1,10 +1,26 @@
-const ProductDetail = ({
-  params,
-}: {
+import { Metadata } from "next";
+
+type Props = {
   params: {
     productId: string;
   };
-}) => {
+};
+
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`IPHONE ${params.productId}`);
+    }, 100);
+  });
+  return {
+    title: `Product Detail of ${title}`,
+    description: `Product Detail of ${params.productId}`,
+  };
+};
+
+const ProductDetail = ({ params }: Props) => {
   const id = params.productId;
   return (
     <div>
