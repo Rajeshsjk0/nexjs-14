cc1858797638761350bbe7175295ecb91339a886
rasubramanian <v-rasubramanian@expediagroup.com>
2024-07-20 20:37:56 +0530

    adding header and footer

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer
+      style={{
+        backgroundColor: "ghostwhite",
+        padding: "1rem",
+      }}
+    >
+      <p>footer</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/header/header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header
+      style={{
+        backgroundColor: "lightblue",
+        padding: "1rem",
+      }}
+    >
+      <p>header</p>
+    </header>
+  );
+};
+
+export default Header;
