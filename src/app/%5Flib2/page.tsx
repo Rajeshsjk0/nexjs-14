b960182cee707eccc86b97492cab8901687de00a
rasubramanian <v-rasubramanian@expediagroup.com>
2024-07-20 20:16:23 +0530

    Private and Public Routes

---
+++ src/app/%5Flib2/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PublicRoute = () => {
+  return (
+    <div>
+      <h1>This is a Public Route this cannot be accessed</h1>
+    </div>
+  );
+};
+
+export default PublicRoute;
---
+++ src/app/_lib/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PrivateRoute = () => {
+  return (
+    <div>
+      <h1>This is a Private Route this cannot be accessed</h1>
+    </div>
+  );
+};
+
+export default PrivateRoute;
